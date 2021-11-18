7bf8f3961826041f2425980eb383be8392124498
aktrooper9 <48301195+aktrooper9@users.noreply.github.com>
2021-11-17 18:30:20 -0800

    lvl 4 :)

--- src/_01_Encapsulate_the_Data/EncapsulateTheData.java
+++ src/_01_Encapsulate_the_Data/EncapsulateTheData.java
@@ -7,7 +7,7 @@ public class EncapsulateTheData {
      * itemsReceived to 0.
      */
 
-    int itemsReceived;
+   private int itemsReceived;
 
     /*
      * degreesTurned must be locked between 0.0 and 360.0 inclusive. All
@@ -15,20 +15,77 @@ public class EncapsulateTheData {
      * bound.
      */
 
-    double degreesTurned;
+    private double degreesTurned;
 
     /*
      * nomenclature must not contain an empty String. An empty String parameter
      * should set nomenclature to a String with a single space.
      */
 
-    String nomenclature;
+    private String nomenclature;
 
     /*
      * memberObj must not be a String. A String parameter should set memberObj
      * to a new Object(); Hint: Use the instanceof operator.
      */
 
-    Object memberObj;
+    private Object memberObj;
+
+	public void setItemsReceived(int randomPositiveNum) {
+		// TODO Auto-generated method stub
+		if(0>=randomPositiveNum) {
+			randomPositiveNum=0;
+		}
+		itemsReceived=randomPositiveNum;
+	}
+
+	public Integer getItemsReceived() {
+		// TODO Auto-generated method stub
+		return itemsReceived;
+	}
+
+	public void setDegreesTurned(double randomPositiveNum) {
+		// TODO Auto-generated method stub
+		if(randomPositiveNum>360) {
+			randomPositiveNum= randomPositiveNum-360;
+		}
+		else if(randomPositiveNum<0) {
+			randomPositiveNum= randomPositiveNum+360;
+		}
+		degreesTurned=randomPositiveNum;
+		}
+	
+
+	public Double getDegreesTurned() {
+		// TODO Auto-generated method stub
+		return degreesTurned;
+	}
+
+	public void setNomenclature(String randomString) {
+		// TODO Auto-generated method stub
+		if(randomString=="") {
+			randomString=" ";
+		}
+		nomenclature=randomString;
+	}
+
+	public Object getNomenclature() {
+		// TODO Auto-generated method stub
+		return nomenclature;
+	}
+
+	public void setMemberObj(Object randomObject) {
+		// TODO Auto-generated method stub
+		if(randomObject instanceof String) {
+			randomObject=new Object();
+		}
+		
+		memberObj=randomObject;
+	}
+
+	public Object getMemberObj() {
+		// TODO Auto-generated method stub
+		return memberObj;
+	}
 
 }
